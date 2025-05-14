7bd8d0255039fdcc14385e99787c7afcb8d7829e
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-05-14 14:27:18 +0530

    discord embed no cache

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import time
 import requests
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, make_response
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -8,7 +8,12 @@ app.url_map.strict_slashes = False
 @app.route("/")
 def csgoanime():
     url = requests.get("https://csgoani.me/api/getnewvideo").json()['video']
-    return render_template("index.html", video_url=url)
+    resp = make_response(render_template("index.html", video_url=url))
+    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    resp.headers['Pragma'] = 'no-cache'
+    resp.headers['Expires'] = '0'
+    return resp
+    # return render_template("index.html", video_url=url)
 
 @app.route("/new")
 def get_video():
