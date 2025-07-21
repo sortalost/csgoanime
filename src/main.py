179696ee57ee8a548f44e0afdd921cc028bd81e5
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-07-21 15:50:01 +0530

    changs

--- src/main.py
+++ src/main.py
@@ -11,38 +11,34 @@ def getvid():
     wrapped = resp.json()
     return json.loads(wrapped['contents'])
 
-@app.route("/t")
-def test():
-    return render_template("t.html")
-
-@app.route('/')
-def _root():
-    data = getvid()
-    video_url = data['video']
-    video_name = video_url.split('/')[-1].split('.')[0]
-    return redirect(url_for('serve_video', videoname=video_name))
+# @app.route('/click')
+# def click():
+#     data = getvid()
+#     video_url = data['video']
+#     video_name = video_url.split('/')[-1].split('.')[0]
+#     return redirect(url_for('serve_video', videoname=video_name))
 
 @app.route('/<videoname>')
 def serve_video(videoname):
     video_url = f"https://csgoani.me/vids/{videoname}.webm"
-    return render_template("index.html", video_url=video_url, video_name=videoname)
+    return render_template("scroll.html", video_url=video_url, video_name=videoname)
 
 @app.errorhandler(404)
-def norfound(e):
+def notfound(e):
     data = getvid()
     video_url = data['video']
     video_name = video_url.split('/')[-1].split('.')[0]
-    return redirect(url_for('serve_video', videoname=video_name))
+    return redirect(url_for('scroll', video_url=video_url, videoname=video_name))
 
 @app.route("/new")
 def get_video():
     data = getvid()
     return jsonify({"video": data['video'], "total": data['num_videos']})
 
-@app.route("/reels")
-def reels():
+@app.route("/")
+def scroll():
     data = getvid()
-    return render_template("reels.html", video_url=data['video'])
+    return render_template("scroll.html", video_url=data['video'])
 
 if __name__ == "__main__":
     app.run()
